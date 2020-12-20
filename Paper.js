b53a0f1957bb09c3fcde78161f920fa341b94da4
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2020-12-20 23:20:58 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,7 +1,7 @@
 class Paper extends BaseClass {
     constructor(x, y, r) {
       super(x,y,r);
-      this.body.image = loadImage("Paper.png");
+      this.image = loadImage("Paper.png");
     }
   }
   
